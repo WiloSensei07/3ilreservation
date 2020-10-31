2f1b6651b6b8d121c44f3de44c9742b213bfffb8
paul mouafo <mouafopauldenilson@gmail.com>
2020-10-31 20:16:37 +0100

    bdd update

--- bd/3ilreservation.sql
+++ bd/3ilreservation.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 11 oct. 2020 à 14:36
--- Version du serveur :  5.7.31-log
--- Version de PHP :  7.4.0
+-- Généré le : sam. 31 oct. 2020 à 19:14
+-- Version du serveur :  8.0.21
+-- Version de PHP : 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `3ilreservation`
+-- Base de données : `3ilreservation`
 --
 
 -- --------------------------------------------------------
@@ -30,11 +29,11 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `creneau`;
 CREATE TABLE IF NOT EXISTS `creneau` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `heure_d` varchar(10) NOT NULL,
   `heure_f` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `creneau`
@@ -54,28 +53,28 @@ INSERT INTO `creneau` (`id`, `heure_d`, `heure_f`) VALUES
 
 DROP TABLE IF EXISTS `horaire`;
 CREATE TABLE IF NOT EXISTS `horaire` (
-  `id` int(11) NOT NULL,
-  `idSalle` int(11) NOT NULL,
+  `id` int NOT NULL,
+  `idsalle` int NOT NULL,
   `date` date NOT NULL,
-  `creneau1` int(11) NOT NULL,
-  `creneau2` int(11) NOT NULL,
-  `creneau3` int(11) NOT NULL,
-  `creneau4` int(11) NOT NULL,
+  `creneau1` int NOT NULL,
+  `creneau2` int NOT NULL,
+  `creneau3` int NOT NULL,
+  `creneau4` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `idSalle` (`idSalle`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `idsalle` (`idsalle`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `horaire`
 --
 
-INSERT INTO `horaire` (`id`, `idSalle`, `date`, `creneau1`, `creneau2`, `creneau3`, `creneau4`) VALUES
+INSERT INTO `horaire` (`id`, `idsalle`, `date`, `creneau1`, `creneau2`, `creneau3`, `creneau4`) VALUES
 (1, 1, '2020-10-02', 1, 0, 0, 1),
-(2, 2, '2020-10-03', 1, 1, 1, 1),
-(3, 3, '2020-10-04', 1, 1, 0, 1),
-(4, 4, '2020-10-04', 1, 0, 1, 1),
-(5, 5, '2020-10-05', 0, 1, 0, 0),
-(6, 6, '2020-10-05', 1, 0, 0, 1),
+(2, 2, '2020-10-02', 1, 1, 1, 1),
+(3, 3, '2020-10-02', 1, 1, 0, 1),
+(4, 4, '2020-10-03', 1, 0, 1, 1),
+(5, 5, '2020-10-03', 0, 1, 0, 0),
+(6, 6, '2020-10-03', 1, 0, 0, 1),
 (7, 7, '2020-10-05', 0, 0, 0, 1),
 (8, 8, '2020-10-05', 1, 1, 1, 1),
 (9, 9, '2020-10-05', 1, 1, 1, 0),
@@ -89,46 +88,44 @@ INSERT INTO `horaire` (`id`, `idSalle`, `date`, `creneau1`, `creneau2`, `creneau
 
 DROP TABLE IF EXISTS `reservation`;
 CREATE TABLE IF NOT EXISTS `reservation` (
-  `id` int(11) NOT NULL,
-  `idUtilisateur` int(11) NOT NULL,
-  `idHoraire` int(11) NOT NULL,
-  `idCreneau` int(11) NOT NULL,
+  `id` int NOT NULL,
+  `idutilisateur` int NOT NULL,
+  `idsalle` int NOT NULL,
+  `date` date NOT NULL,
+  `creneau` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `idUtilisateur` (`idUtilisateur`),
-  KEY `idHoraire` (`idHoraire`),
-  KEY `idCreneau` (`idCreneau`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `idutilisateur` (`idutilisateur`),
+  KEY `idsalle` (`idsalle`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `reservation`
 --
 
-INSERT INTO `reservation` (`id`, `idUtilisateur`, `idHoraire`, `idCreneau`) VALUES
-(1, 1, 1, 1),
-(2, 2, 2, 3),
-(3, 3, 3, 4),
-(4, 4, 4, 3),
-(5, 5, 2, 2);
+INSERT INTO `reservation` (`id`, `idutilisateur`, `idsalle`, `date`, `creneau`) VALUES
+(1, 1, 1, '2020-10-02', '08h30-10h00'),
+(2, 2, 2, '2020-10-02', '15h15-16h45'),
+(3, 3, 3, '2020-10-03', '08h30-10h00');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `salles`
+-- Structure de la table `salle`
 --
 
-DROP TABLE IF EXISTS `salles`;
-CREATE TABLE IF NOT EXISTS `salles` (
-  `id` int(11) NOT NULL,
-  `numero` int(11) NOT NULL,
-  `nb_place` int(11) NOT NULL,
+DROP TABLE IF EXISTS `salle`;
+CREATE TABLE IF NOT EXISTS `salle` (
+  `id` int NOT NULL,
+  `numero` int NOT NULL,
+  `nbplace` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Déchargement des données de la table `salles`
+-- Déchargement des données de la table `salle`
 --
 
-INSERT INTO `salles` (`id`, `numero`, `nb_place`) VALUES
+INSERT INTO `salle` (`id`, `numero`, `nbplace`) VALUES
 (1, 100, 9),
 (2, 101, 0),
 (3, 102, 16),
@@ -148,46 +145,29 @@ INSERT INTO `salles` (`id`, `numero`, `nb_place`) VALUES
 
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `login` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` varchar(8) NOT NULL,
+  `code_secret` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`id`, `login`, `password`, `role`) VALUES
-(1, 'test@3il.fr', '$2y$10$.vRtbtz2cPSVoo7iixYr.OHJHuygY8Db6aLtKZbM2411s0UyzyvU6', 'etudiant'),
-(2, 'erat.eget.tincidunt@etrutrum.net', '$2y$10$mBZxOyf2kiTCeMR9y2qBgOCzh43AgjZcxIUfebQGZUhpOMt7RHz1e', 'etudiant'),
-(3, 'non.bibendum@commodoipsumSuspendisse.net', '$2y$10$wg19kTnZ6SUJ4OVNaIX2Ce0O1COoX8rcK8oD7wyMuQinBd33mRjfS', 'etudiant'),
-(4, 'volutpat.Nulla@parturientmontes.net', '$2y$10$MaQfQw2ziLZfj1JgXIaDEOftzCN3nUcSB/GHBog4OplAf25L3gasa', 'etudiant'),
-(5, 'ornare@egestasligulaNullam.ca', '$2y$10$iImzYtUYgxdwe6ul1iGcxuGO25ORookO.MWMivl.hK2xoXxIyAH6m', 'etudiant'),
-(6, 'ut.ipsum@sapien.net', '$2y$10$SmRsep6lqe1NpZdbkwTDM.Fw94BbGaAGnc9ICUKfh9/HNIbLW4psS', 'etudiant'),
-(7, 'sollicitudin.adipiscing@eleifendnec.co.uk', '$2y$10$rtjFmEzcKqY7W6zX8w3/juCZrGYz2ugLagzcugBH4OcaBNVZPzbO6', 'etudiant'),
-(8, 'varius.orci@risusMorbi.net', '$2y$10$Zkc7In/13jD/Ox2H0xbLCe/P3SU1DlT/8rUyiYHSrgDAHTGJt2U2.', 'etudiant'),
-(9, 'tellus.eu@semsemper.net', '$2y$10$bCdOgCz1FWZCRpLT1WCRAe1qgqEvw0TmeIY1eLQOhGw09B92aJz/W', 'etudiant'),
-(10, 'admin@3il.fr', '$2y$10$h1uzPfOWthPBj4ssxkYfdu864A4E2fOOWVz8nvizMa7CPUxMShdRi', 'admin');
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `horaire`
---
-ALTER TABLE `horaire`
-  ADD CONSTRAINT `horaire_ibfk_1` FOREIGN KEY (`idSalle`) REFERENCES `salles` (`id`);
-
---
--- Contraintes pour la table `reservation`
---
-ALTER TABLE `reservation`
-  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`),
-  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`idHoraire`) REFERENCES `horaire` (`id`),
-  ADD CONSTRAINT `reservation_ibfk_3` FOREIGN KEY (`idCreneau`) REFERENCES `creneau` (`id`);
+INSERT INTO `utilisateur` (`id`, `login`, `password`, `role`, `code_secret`) VALUES
+(1, 'test@3il.fr', '$2y$10$KamOCWd7MJ45hd6Amkw4he2kF2JfIz0VtRqCR7clXBIMlNBnQRP.O', 'etudiant', 'ZKA-XZB-YCR-DGF-WMK-FVX'),
+(2, 'erat.eget.tincidunt@etrutrum.net', '$2y$10$wiQ48ZuZyuTP/Vlzk8XYZ.ic4eevjAu0awkYmMxMbrPL6CE4kbAKq', 'etudiant', 'ZKA-DGF-FVX-AAB-BAD-ACD'),
+(3, 'non.bibendum@commodoipsumSuspendisse.net', '$2y$10$Cc.B4ONpz54yg.w61uUUa.dBjZirHuo/1dopuIXRapI24c9yp0ATC', 'etudiant', 'YCR-XZB-ZKA-ACD-BAD-AAB'),
+(4, 'volutpat.Nulla@parturientmontes.net', '$2y$10$mfT2Oz9hDM4hfyx425AFZecaj/GxchX.0o0LMZ445iZIBivAV5kCq', 'etudiant', '\'WMK-DGF-YCR-XZB-ZKA-ACD'),
+(5, 'ornare@egestasligulaNullam.ca', '$2y$10$AlePS7FmwkyCZHkPrOctfu9eufcP/wbC/02dadb9nLmLP957qdZ4O', 'etudiant', 'OYC-FVX-WMK-DGF-YCR-XZB'),
+(6, 'ut.ipsum@sapien.net', '$2y$10$1558969j/oxOBZuL1PhiS.MxfIYguRzINeGwBTwcxfKINJeDahdz6', 'etudiant', 'FVX-WMK-DGF-YCR-XZB-ZKA'),
+(7, 'sollicitudin.adipiscing@eleifendnec.co.uk', '$2y$10$kYwx0N7reU4nAAyA0LMMO.gVNeLB/7OD8ruDtxymZh2QqbgpZB5ji', 'etudiant', 'DGF-YCR-XZB-ZKA-ACD-BAD'),
+(8, 'varius.orci@risusMorbi.net', '$2y$10$rHSqEcWndUl9G5cs60y3cOCW00z3dkEGVDqlnAo5V0u.6RFPpB1/K', 'etudiant', 'DGF-WMK-YCR-XZB-ZKA-ACD'),
+(9, 'tellus.eu@semsemper.net', '$2y$10$RHry.SV27nvmKSgEYWd0tenh1K3Y74XWt1oDYovlUFCEZpYA8EBh6', 'etudiant', 'BAD-ACD-ZKA-ZKA-YCR-YCR'),
+(10, 'admin@3il.fr', '$2y$10$KqPJ9UBd2P4050RiHBuIB.2VaASzgfUpzdicllK.xYUy5Apke5fJG', 'admin', 'BAD-ACD-ZKA-XZB-YCR-DGF');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

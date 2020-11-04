3f0bfdc2e0cbe4dc4688855ff280a9c3a56dbafa
paul mouafo <mouafopauldenilson@gmail.com>
2020-11-04 23:04:17 +0100

    admin backend et inscription user

--- view/homeadmin.php
+++ view/homeadmin.php
@@ -1,78 +1,53 @@
-<br><h3 class="text-center" style="color: black"> Gestion des salles de TP </h3><br>
+<div id="formulaire">
 
-<div style="margin-left: 20px; margin-right: 20px;">
+    <br><h3 class="text-center" style="color: black"> Gestion des salles de TP </h3><br>
 
-    <div class="form-group row text-center ">
-        <div class="form-group row">
-            <label for="example-date-input" class="col-2 col-form-label">Date</label>
-            <div class="col-10">
-                <input class="form-control" type="date">
-            </div>
+    <div id="configurationSalle" style="margin-left: 20px; margin-right: 20px;">
+
+        <div class="row text-center">
+            <h2 style="color: #0CA60C;"> Veillez choisir une salle pour pouvoir mettre a jour ses horaires !</h2>
         </div>
-        <pre>             </pre>
-        <div class="form-group row">
-            <label for="example-date-input" class="col-8 col-form-label">Place total</label>
-            <div class="col-4">
-                <input class="form-control" type="text" value="0" >
-            </div>
-        </div>
-    </div>
 
-
-    <div class="row text-center">
-        <table class="table table-bordered">
-            <thead>
-                <tr class="table-primary">
-                    <th scope="col">Horaires</th>
-                    <th scope="col">08h30 - 10h00</th>
-                    <th scope="col">10h30 - 12h00</th>
-                    <th scope="col">13h30 - 15h00</th>
-                    <th scope="col">15h15 - 16h45</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="table-secondary">
-                    <th scope="row">Lundi</th>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-check" style="color: #0CA60C;"></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-times" style="color: red;"></i></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-check" style="color: #0CA60C;"></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-times" style="color: red;"></i></button></td>
-                </tr>
-                <tr>
-                    <th scope="row">Mardi</th>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-check" style="color: #0CA60C;"></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-check" style="color: red;"></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-check" style="color: #0CA60C;"></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-times" style="color: red;"></i></button></td>
-                </tr>
-                <tr class="table-secondary">
-                    <th scope="row">Mercredi</th>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-times" style="color: red;"></i></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-check" style="color: #0CA60C;"></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-times" style="color: red;"></i></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-times" style="color: red;"></i></button></td>
-                </tr>
-                <tr>
-                    <th scope="row">Jeudi</th>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-check" style="color: #0CA60C;"></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-check" style="color: #0CA60C;"></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-times" style="color: red;"></i></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-check" style="color: #0CA60C;"></button></td>
-                </tr>
-                <tr class="table-secondary">
-                    <th scope="row">Vendredi</th>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-times" style="color: red;"></i></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-check" style="color: #0CA60C;"></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-times" style="color: red;"></i></button></td>
-                    <td><button class="btn-outline-secondary"><i class="fas fa-check" style="color: #0CA60C;"></button></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-
-    <div class="row float-right" style="margin: 10px;">
-        <button type="button" class="btn btn-secondary" style="margin-right: 10px;">Annuler</button>
-        <button type="button" class="btn btn-primary">Enregistrer</button>
     </div>
 
 </div>
+
+
+<script>
+    function horaireSalle(date)
+    {
+        let idsalle = document.getElementById('idsalle').value ;
+        document.getElementById('dateHoraire').value = date;
+        let obj = {date: date, idSalle: idsalle, confirm: 1}
+
+        $.ajax({
+            url: "../controller/gestionSalle.php",
+            type:"GET",
+            data:obj
+        }).done(function( arg ) {
+            document.getElementById('configurationSalle').innerHTML = arg
+            //location.reload(true);
+        });
+    }
+</script>
+<script src="../js/changerSatusSalle.js"></script>
+
+<script>
+    function confirmer()
+    {
+        let idsalle = document.getElementById('idsalle').value ;
+        let date = document.getElementById('dateHoraire').value ;
+        let nbplace = document.getElementById('nbplaceSalle').value ;
+
+
+        let obj = {date: date, idSalle: idsalle, nbplace: nbplace, confirm: 3}
+
+        $.ajax({
+            url: "../controller/gestionSalle.php",
+            type:"GET",
+            data:obj
+        }).done(function( arg ) {
+            location.reload(true);
+        });
+    }
+</script>
